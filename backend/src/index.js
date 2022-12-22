d6030d4150829fd679b7fe66d3edfd1e4fe0474c
DCM91 <castromartindaniel91@gmail.com>
2022-12-22 14:54:30 +0100

    añadido cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import mongo from './Mongo/index.js';
 
 
@@ -7,6 +8,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
+app.use(cors());
 
 app.listen(port, () => {
     console.log(`Server is up and running at port ${port} ⚡`)
