30c3cc18736d466495490a9428e41d8391872309
Jorge Cuesta <jorgecuesta.90@gmail.com>
2022-12-29 18:21:40 +0100

    add funcionalidad

--- frontend/src/Componentes/FormularioUsuario/formUser.js
+++ frontend/src/Componentes/FormularioUsuario/formUser.js
@@ -1,8 +1,53 @@
 import React from "react";
 import styles from "./formUser.module.css"
 
-const FormUser = () =>{
+const FormUser = (props) =>{
+    [username, setUsername] = useState("")
+    [email, setEmail] = useState("")
+    [name, setName] = useState("")
+    [lastname, setLastname] = useState("")
 
+    const getUsername = (event) =>{
+        setUsername(event.target.value)
+    }
+
+    const getEmail = (event) =>{
+        setEmail(event.target.value)
+    }
+
+    const getName = (event) =>{
+        setName(event.target.value)
+    }
+    const getLastname = (event) =>{
+        setLastname(event.target.value)
+    }
+
+    const postUser = () =>{
+        const url = "http://localhost:3001/user";
+        const body ={
+            userName: username,
+            email,
+            name,
+            lastName: lastname
+        };
+        const options = {
+            method: "POST",
+            mode: "cors",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+            },
+            body: JSON.stringify(body),
+          };
+          fetch(url,options)
+          .then((res)=>{
+            res.json();
+          })
+          .then((data)=>{
+            console.log(data)//aqui entiendo que cuando se cree el homepage de user habra que
+                            //redirigir para que cargue el componente del perfil creado. => la home de ese perfil.
+          })
+    }
     return(
         <div className={styles.card}>
             <h1>Bienvenido a SKUADLACK</h1>
@@ -11,21 +56,26 @@ const FormUser = () =>{
                 
                 <label for="userName">
                    userName 
-                   <input id="userName" type="text" required/>
+                   <input id="userName" type="text" required onChange={getUsername}/>
                    <p>Este campo será tu nombre visible en SKUADLACK.</p>
                 </label>
                 <label for="email">
                    email 
-                <input id="email" type="text" required/>
+                <input id="email" type="text" required onChange={getEmail}/>
                    <p>Te recomendamos que te registres con el email del trabajo.</p>
                 </label>
                 <label for="name">
                    name 
-                   <input id="name" type="text" required/>
+                   <input id="name" type="text" required onChange={getName}/>
                    <p>Identificate con tu verdadero nombre.</p>
                 </label>
-                <button type="submit">Enviar</button>
-
+                <button type="submit" onClick={postUser}>Enviar</button>
+                <label for="lastName">
+                   last name 
+                   <input id="lastName" type="text" required onChange={getLastname}/>
+                   <p>Identificate con tu verdadero apellido.</p>
+                </label>
+                <button type="submit" onClick={postUser}>Enviar</button>
 
             </form>
         </div>
