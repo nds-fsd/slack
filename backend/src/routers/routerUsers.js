e026d2ac37e067d0a8189629e8223d99809f65bb
Jorge Cuesta <jorgecuesta.90@gmail.com>
2023-01-03 17:48:42 +0100

    add .get in routerUsers.js

--- backend/src/routers/routerUsers.js
+++ backend/src/routers/routerUsers.js
@@ -1,2 +1,18 @@
 const express = require('express');
+const { default: User } = require('../Schemas/user');
 const userRouter = express.Router();
+
+
+userRouter.get('/user/:id', async (req,res)=>{
+    const id = req.params.id
+    try{
+    const user = await User.findById(id)
+    if (user){
+        res.status(200).json(user)
+    }else{
+        res.status(404).send('No existe este usuario')
+    }}catch(error){
+        res.status(500).json(error)
+    }
+});
+
