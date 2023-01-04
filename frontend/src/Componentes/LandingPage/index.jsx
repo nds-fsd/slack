f3f4d7b0959ab2a43b65f820e6429b52ec460534
Germán Cachafeiro Bamonde <gercaba@mizu.local>
2023-01-04 23:44:52 +0100

    init2

--- frontend/src/Componentes/LandingPage/index.jsx
+++ frontend/src/Componentes/LandingPage/index.jsx
@@ -5,13 +5,22 @@ import styles from "./index.module.css"
 const LandingPage = () =>{
 
 
+
     return (
         <div className={styles.seccion}>
-            <div className={styles.seccion1}></div>
-            <div className={styles.seccion2}></div>
-            <div className={styles.seccion3}></div>
-            <div className={styles.seccion4}></div>
-            <div className={styles.seccion5}></div>
+
+            <div className={styles.navBar}>Barra de navegación superior</div>
+            
+            <div className={styles.seccion1}>
+                <video src="/Users/gercaba/Documents/Nuclio/skuadlack/slack/frontend/src/Componentes/Media/480_verde.mov" loop={true} autoPlay > </video>
+            
+            
+            </div>
+
+            <div className={styles.seccion2}>sección 2</div>
+            <div className={styles.seccion3}>sección 3</div>
+            <div className={styles.seccion4}>sección 4</div>
+            <div className={styles.seccion5}>sección 5</div>
 
         </div>   
     
