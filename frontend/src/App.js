74b42cde86ddda86e94deac0577b682343ce7304
Germán Cachafeiro Bamonde <gercaba@mizu.local>
2023-01-04 22:31:43 +0100

    init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import FormUser from './Componentes/FormularioUsuario/formUser';
 import FormOrganizacion from './Componentes/FormularioOrganizacion/formOrganizacion.jsx';
+import LandingPage from './Componentes/LandingPage';
 
 
 
@@ -8,8 +9,7 @@ function App() {
 
   return (
     <div>
-      <FormUser/>
-      <FormOrganizacion/>
+      <LandingPage/>
 
     </div>
   );
---
+++ frontend/src/Componentes/LandingPage/index.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import styles from "./index.module.css"
+
+
+const LandingPage = () =>{
+
+
+    return (
+        <div className={styles.seccion}>
+            <div className={styles.seccion1}></div>
+            <div className={styles.seccion2}></div>
+            <div className={styles.seccion3}></div>
+            <div className={styles.seccion4}></div>
+            <div className={styles.seccion5}></div>
+
+        </div>   
+    
+    )}
+
+
+export default LandingPage
