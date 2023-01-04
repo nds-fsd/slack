afc1c929e7f2eb9d29db3095700a4d382f1f874c
DCM91 <castromartindaniel91@gmail.com>
2023-01-04 21:10:35 +0100

    mejorando enrutamiento

--- backend/src/routers/routerOrg.js
+++ backend/src/routers/routerOrg.js
@@ -12,21 +12,26 @@ routerOrg.get('/organizacion',async(req,res)=>{
 });
 
 routerOrg.post('/organizacion', async(req,res)=> {
-    try{
     const body = req.body;
     const data = {
-        OrgName: body.OrgName,
-        OrgMail: body.OrgMail,
-        OrgDescription: body.OrgDescription
+      OrgName: body.OrgName,
+      OrgMail: body.OrgMail,
+      OrgDescription: body.OrgDescription
+    };
+  
+    if (!data.OrgName || !data.OrgMail) {
+      return res.status(422).send("Falta el nombre o el correo electrónico de la organización");
     }
-    const organizacion = new Organizacion(data);
-    await organizacion.save();
-    res.status(201).json(organizacion)
-}catch(error){
-    res.status(500).json(error)
-}
-});
-
+  
+    try {
+      const organizacion = new Organizacion(data);
+      await organizacion.save();
+      res.status(201).json(organizacion);
+    } catch(error) {
+      res.status(400).send(error.message);
+    }
+  });
+  
 routerOrg.get('/organizacion/:id', async (req,res)=>{
     const id = req.params.id
     try{
@@ -54,14 +59,21 @@ routerOrg.patch('/organizacion/:id', async(req,res)=>{
 });
 
 routerOrg.delete('/organizacion/:id', async(req,res)=>{
-    const id = req.params.id
-    try{
-        const organizacionDelete = await Organizacion.findByIdAndDelete(id)
-        res.status(204).send("Deleted")
-    }catch(error){
-        res.status(500).json(error)
+  const id = req.params.id;
+
+  try {
+    const organizacion = await Organizacion.findById(id);
+    if (!organizacion) {
+      return res.status(404).send("Recurso no encontrado");
     }
+
+    await organizacion.delete();
+    res.send("Recurso eliminado con éxito");
+  } catch(error) {
+    res.status(500).json(error);
+  }
 });
+
 
 
 
