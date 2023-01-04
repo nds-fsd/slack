46ebbb5e8e3cca6fd4557d51f21675f4f7e0d31c
Germán Cachafeiro Bamonde <gercaba@mizu.local>
2023-01-04 16:43:17 +0100

    vFinal

--- frontend/src/Componentes/FormularioOrganizacion/formOrganizacion.jsx
+++ frontend/src/Componentes/FormularioOrganizacion/formOrganizacion.jsx
@@ -20,13 +20,12 @@ const FormOrganizacion = () =>{
     }
 
     const postOrganizaciones = () =>{
-        const url = "http://localhost:3001/organizacion";
+        const url = "http://localhost:3001/organizacion"; //pendiente saber la ruta
         const body ={
             OrgMail: email,
             OrgName: name,
             OrgDescription: trabajoActual
-            //Saber exactamente las nomenclaturas del backend
-
+         
         };
         const options = {
             method: "POST",
@@ -56,15 +55,15 @@ const FormOrganizacion = () =>{
         <div id='formOrg'>
             <h1 className="tituloFormOrg" id="tituloCorreo">Primero, introduce tu correo electrónico</h1>
             <p className="comentarioCorreo">Te sugerimos que uses la<b className="comentarioCorreo"> dirección de correo electrónico que usas en el trabajo</b></p>
-            <input className="inputFormOrg" id = "inputCorreo" type = "text" placeholder="nombre@work-email.com"  onChange={getEmail}></input>
+            <input className="inputFormOrg" id = "inputCorreo" type = "text" placeholder="nombre@work-email.com"  required onChange={getEmail}></input>
 
             <h1 className="tituloFormOrg" id="tituloNombreOrg">¿Cómo se llama tu fantástico equipo?</h1>
-            <input className="inputFormOrg" id = "inputNombreOrg" type = "text" placeholder="Avengers" onChange={getName} ></input>
+            <input className="inputFormOrg" id = "inputNombreOrg" type = "text" placeholder="Avengers" required onChange={getName} ></input>
 
             <h1 className="tituloFormOrg" id="tituloTrabajoActual">¿En qué está trabajando tu equipo actualmente?</h1>
-            <input className="inputFormOrg" id = "inputTrabajoActual" type = "text" placeholder="Ocupados salvando al mundo" onChange={getTrabajoActual}></input>
+            <input className="inputFormOrg" id = "inputTrabajoActual" type = "text" placeholder="Ocupados salvando al mundo" required onChange={getTrabajoActual}></input>
 
-            <button className="button" id='butFormOrg' onClick={postOrganizaciones}>Continuar</button>
+            <button type="submit" className="button" id='butFormOrg' onClick={postOrganizaciones}>Continuar</button>
             
         </div>
         
