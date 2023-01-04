17db099d07fb5db7b14c3fc956d4ee67101d5bc1
DCM91 <castromartindaniel91@gmail.com>
2023-01-04 20:21:17 +0100

    eliminando merge a main por error...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
-import FormUser from './Componentes/FormularioUsuario/formUser';
-import FormOrganizacion from './Componentes/FormularioOrganizacion/formOrganizacion.jsx';
-
 
 
 function App() {
 
   return (
     <div>
-      <FormUser/>
-      <FormOrganizacion/>
 
     </div>
   );
