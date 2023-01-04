9b01cddfdedab9dc7b0dc5bfd629eeda13fb476f
Germán Cachafeiro Bamonde <gercaba@mizu.local>
2023-01-04 12:18:24 +0100

    pendiente revisión

--- frontend/src/Componentes/FormularioOrganizacion/formOrganizacion.jsx
+++ frontend/src/Componentes/FormularioOrganizacion/formOrganizacion.jsx
@@ -6,14 +6,14 @@ import Styles from "./formOrganizacion.css"
 const FormOrganizacion = () =>{
 
     const [email, setMail] = useState("");
-    const [titulo, setTitulo] = useState("");
+    const [name, setName] = useState("");
     const [trabajoActual, setTrabajoActual] = useState ("");
 
     const getEmail = (event)=> {
         setMail(event.target.value)
     }
-    const getTitulo= (event)=> {
-        setTitulo(event.target.value)
+    const getName= (event)=> {
+        setName(event.target.value)
     }
     const getTrabajoActual = (event)=> {
         setTrabajoActual(event.target.value)
@@ -23,7 +23,7 @@ const FormOrganizacion = () =>{
         const url = "http://localhost:3001/organizacion";
         const body ={
             OrgMail: email,
-            OrgMail: titulo,
+            OrgName: name,
             OrgDescription: trabajoActual
             //Saber exactamente las nomenclaturas del backend
 
@@ -59,7 +59,7 @@ const FormOrganizacion = () =>{
             <input className="inputFormOrg" id = "inputCorreo" type = "text" placeholder="nombre@work-email.com"  onChange={getEmail}></input>
 
             <h1 className="tituloFormOrg" id="tituloNombreOrg">¿Cómo se llama tu fantástico equipo?</h1>
-            <input className="inputFormOrg" id = "inputNombreOrg" type = "text" placeholder="Avengers" onChange={getTitulo} ></input>
+            <input className="inputFormOrg" id = "inputNombreOrg" type = "text" placeholder="Avengers" onChange={getName} ></input>
 
             <h1 className="tituloFormOrg" id="tituloTrabajoActual">¿En qué está trabajando tu equipo actualmente?</h1>
             <input className="inputFormOrg" id = "inputTrabajoActual" type = "text" placeholder="Ocupados salvando al mundo" onChange={getTrabajoActual}></input>
