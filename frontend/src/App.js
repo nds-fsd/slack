797af6c45a876778970358ea2b184ac4c70de586
Germán Cachafeiro Bamonde <gercaba@mizu.local>
2023-01-07 11:50:50 +0100

    Enrutamiento OK

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,40 @@ import './App.css';
 import FormUser from './Componentes/FormularioUsuario/formUser';
 import FormOrganizacion from './Componentes/FormularioOrganizacion/formOrganizacion.jsx';
 import LandingPage from './Componentes/LandingPage';
+import {Routes, Route, Link} from "react-router-dom"
+import styles from "./Styles/App.module.css"
+import logo from "./Assets/logo.png"
 
-
+//react router dom está en la clase de React Router. Webpack y Eslint --> minuto 01:13:00
 
 function App() {
 
   return (
     <div>
-      <LandingPage/>
+      <div className={styles.navBar}>
+
+                    <Link to="/"><img className={styles.logo} src={logo} alt='logo'/></Link>
+                    <Link to="/"> SkuadLack</Link>
+                    <a> Producto</a>
+                    <a> Enterprise</a>
+                    <a> Recurso</a>
+                    <a> Precios</a> 
+                    <Link to="/user"> Log in</Link> 
+                    <Link to='/organizacion'> Crear organización</Link>
+               
+            </div>
+
+      
+    <div className='main-router'>
+      <Routes>
+
+        <Route path="/organizacion" element={<FormOrganizacion />}>  </Route>
+        <Route path="/user" element={<FormUser/>}>  </Route>
+        <Route path="/" element={<LandingPage/>}></Route>
+
+      </Routes>
+
+      </div>
 
     </div>
   );
--- frontend/src/Componentes/LandingPage/index.jsx
+++ frontend/src/Componentes/LandingPage/index.jsx
@@ -3,6 +3,9 @@ import styles from "./index.module.css";
 import videoVerde from '../../Assets/480_verde.mp4';
 import videoMarron from '../../Assets/480_marron.mp4';
 import logo from '../../Assets/logo.png';
+import {Routes, Route, Link} from "react-router-dom"
+import FormOrganizacion from "../FormularioOrganizacion/formOrganizacion";
+import FormUser from "../FormularioUsuario/formUser";
 
 const LandingPage = () =>{
 
@@ -12,23 +15,12 @@ const LandingPage = () =>{
 
         <div>
 
-            <div className={styles.navBar}>
-                    <img className={styles.logo} src={logo} alt='logo'/>
-                    <a> SkuadLack</a>
-                    <a> Producto</a>
-                    <a> Enterprise</a>
-                    <a> Recurso</a>
-                    <a> Precios</a> 
-                    <a> Log in</a> 
-                    <a> Crear organización</a>
-               
-            </div>
-            
+                      
             <div className={styles.seccion1}>
                 <div className={styles.seccion1_1}> 
                     <h1>Una aplicación para tomarte las cosas con calma</h1>
                     <p> Reúne a tus amigos y compañeros de trabajo y dedica tiempo a todo aquello que sume valor. Reduce las reuniones, mensajes y correos.</p>
-                    <button type="submit" >Regístrate con un correo</button>
+                    <Link to="/user"><button type="submit" to="/user">Regístrate con un correo</button> </Link>
                     <p> <b>SkuadLack es gratuíto</b> y no se cambiarán las políticas de precios como hace Movistar</p>
                 </div>
                 <div className={styles.contenedorVideo}>
@@ -47,7 +39,11 @@ const LandingPage = () =>{
             <div className={styles.seccion4}>sección 4</div>
             <div className={styles.seccion5}>sección 5</div>
 
-        </div>   
+            <Routes>
+                <Route path="/user" element={<FormUser/>}></Route>
+            </Routes>
+
+        </div> 
     
     )}
 
