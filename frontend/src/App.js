6bcb892f78397dd7344db9ee1b3d6ee02a9afbc5
Jorge Cuesta <jorgecuesta.90@gmail.com>
2023-01-08 18:03:49 +0100

    recolocado rutas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import styles from "./Styles/App.module.css"
 import logo from "./Assets/logo.png"
 import DashboardUser from './Componentes/DashboardUsuario/dashboardUser';
 import NotFound from './Componentes/NotFound/notFound';
+import FormUser1 from './Componentes/FormularioUsuario/formUser1';
 
 //react router dom está en la clase de React Router. Webpack y Eslint --> minuto 01:13:00
 
@@ -17,26 +18,29 @@ function App() {
       <div className={styles.navBar}>
 
                     <Link to="/"><img className={styles.logo} src={logo} alt='logo'/></Link>
-                    <Link to="/" className='link'> <a>SkuadLack</a></Link>
-                    <a> Producto</a>
+                    <Link to="/" className='link'>SkuadLack</Link>
+                    <Link to="/producto" className='link'> Producto</Link>
+                    <Link to="/enterprise" className='link'> Enterprise</Link>
+                    <Link to="/recurso" className='link'>Recurso</Link>
+                    <Link to="/precios" className='link'>Precios</Link>
+                    {/* <a> Producto</a>
                     <a> Enterprise</a>
                     <a> Recurso</a>
-                    <a> Precios</a> 
-                    <Link to="/user" className='link'><a> Log in</a></Link> 
-                    <Link to='/organizacion' className='link'> <a>Crear organización</a></Link>
+                    <a> Precios</a>  */}
+                    <Link to="/user" className='link'>Log in</Link> 
+                    <Link to='/organizacion' className='link'>Crear organización</Link>
                
             </div>
 
       
     <div className='main-router'>
       <Routes>
-
-        <Route path="/organizacion" element={<FormOrganizacion />}>  </Route>
-        <Route path="/user" element={<FormUser/>}>  </Route>
         <Route path="/" element={<LandingPage/>}></Route>
+        <Route path="/organizacion" element={<FormOrganizacion />}>  </Route>
+        <Route path="/user" element={<FormUser1/>}>  </Route>
         <Route path="/user/:id" element={<DashboardUser/>}>  </Route>
-        <Route path="*" element={<NotFound/>}/>
-
+        <Route path="/user/undefined" element={<NotFound/>}/>
+        {/* <Route path="/*" element={<NotFound/>}/> */}
       </Routes>
 
       </div>
