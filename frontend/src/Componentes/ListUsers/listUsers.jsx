af2495056d673ea6a38b78258cfc45b668084bfc
Jorge Cuesta <jorgecuesta.90@gmail.com>
2023-01-12 21:23:03 +0100

    correction error loop infinito

--- frontend/src/Componentes/ListUsers/listUsers.jsx
+++ frontend/src/Componentes/ListUsers/listUsers.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import styles from "./listUsers.module.css"
 
 const ListUsers = ()=>{
-
-    const [list, setList]=useState([]);
+        const [list, setList]=useState([]);
 
     useEffect (()=> {
         fetch("http://localhost:3001/user")
@@ -14,7 +14,34 @@ const ListUsers = ()=>{
                 setList(res);
             })
             console.log('Acabado el fetch');
-    },[list])
+    },[])
+    
+   
+      
+    
+    
+
+    // const deleteUser = (datosTabla) =>{
+    //     const url = "http://localhost:3001/user/" + datosTabla._id
+    //     const options = {
+    //       method: "DELETE",
+    //       mode: "cors",
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //         "Accept": "application/json",
+    //       }
+    //     };
+    //     fetch(url, options)
+    //       .then((res) => {
+    //         res.json();
+    //       })
+    //       .then(() => {
+    //         alert(`Usuario  eliminado.`)
+           
+    //       });
+    //       }
+
+
 
     return(
 
@@ -37,7 +64,7 @@ const ListUsers = ()=>{
                     <td> {datosTabla.userName}</td>
                     <td> {datosTabla.email}</td>
                     <td> {datosTabla.lastName}</td>
-                    <td className={styles.botones}><button className={styles.butEdit}>Editar</button></td>
+                    <td className={styles.botones}><Link to={`../user/${datosTabla._id}`}><button className={styles.butEdit}>Editar</button></Link></td>
                     <td className={styles.botones}><button className={styles.butEliminar}>Eliminar</button></td>
                 </tr>   
                 
@@ -54,8 +81,10 @@ export default ListUsers
 Pendiente:
 1. Hacer que el scroll de la tabla no supere el encabezado. Es decir que no puedean subir las filas por encima del encabezado
 2. Poner bordes al encabezado cuando haga scroll.
-3. Añadir la funcionalidad de edición y borrado de un usuario
+3. borrado de un usuario// intento de eliminar usuario y creo que por que hay un map se borran todos, no entiendo por que
 4. Aplicar la misma lógica a organizaciones
 5. Meter ambas funcionalidades en un menú lateral que definal las acciones que puede realizar el admin
+6.Al poner list en la array de dependencias loop infinito lograr que la pagina si hay un cambio en la lista de usuarios
+actualice al momento, asi se conseguia pero claro loop infinito 
 
 */
