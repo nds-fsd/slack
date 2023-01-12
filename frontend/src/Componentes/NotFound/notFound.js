c4f3775b2cd32294129889eac350feafdcb3b741
Jorge Cuesta <jorgecuesta.90@gmail.com>
2023-01-12 19:22:40 +0100

    add image at NotFound component

--- frontend/src/Componentes/NotFound/notFound.js
+++ frontend/src/Componentes/NotFound/notFound.js
@@ -1,15 +1,17 @@
 import { BarraNav } from '../BarraNav/BarraNav';
 import styles from './notFound.module.css'
+import notFound from '../../Assets/notFound.png'
 
-const NotFound = () =>{
-    return(
+const NotFound = () => {
+    return (
         <>
-        <BarraNav />
-        <div className={styles.card}>
-            <h3>Ups!! algo ha fallado.</h3>
-        </div>
+            <BarraNav />
+            <div className={styles.card}>
+                <img src={notFound} /><br/>
+                <h3>Ups!! algo ha fallado.</h3>
+            </div>
         </>
-       
+
     )
 }
 export default NotFound;
