f6e33828ed38a3a00474a6ce4a909c0558b2a9f5
Germán Cachafeiro Bamonde <gercaba@mizu.local>
2023-01-16 18:48:16 +0100

    actualizado el archivo index.js que no estaba completo

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import mongo from './Mongo/index.js';
+import routerUsers from './routers/routerUsers.js';
+import routerOrg from './routers/routerOrg.js';
 
 
 
@@ -12,7 +14,8 @@ const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
-
+app.use(routerUsers);
+app.use(routerOrg)
 
 app.listen(port, () => {
     console.log(`Server is up and running at port ${port} ⚡`)
