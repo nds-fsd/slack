3c6c8fd90ff013257dd81ff5232820a642ddcc57
Germán Cachafeiro Bamonde <gercaba@mizu.local>
2023-01-20 17:08:39 +0100

    inicio Modal para User

---
+++ frontend/src/Componentes/Modal/modal.jsx
@@ -0,0 +1,34 @@
+
+import React, { useEffect, useRef } from "react";
+import styles from './modal.module.css'
+
+const Modal = ( {children, onClose, open }) => {
+    const modalReference = useRef(null);
+
+    useEffect(() => {
+
+        const handleClickOutside = (e) => {
+            if (modalReference.current && !modalReference.current.contains(e.target) && open) {
+                onClose();
+            }
+        };
+
+        document.addEventListener('click', handleClickOutside, true);
+
+        return () => {
+            document.removeEventListener('click', handleClickOutside, true);
+        };
+    }, [modalReference, open]);
+
+    return (
+        <div className={`${styles.backdrop} ${open && styles.open}`}>
+        <div
+          className={`${styles.modal} ${open && styles.open}`}
+          ref={modalReference}
+        >
+          {children}
+        </div>
+      </div>
+
+    )
+}
