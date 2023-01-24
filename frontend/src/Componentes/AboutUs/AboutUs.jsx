ba34d3dc3a16f47ea8ab281dc3fabe5e8a0ee5bf
Nicoga96 <nicogaga2@gmail.com>
2023-01-24 18:18:00 +0100

    Cambios about us

--- frontend/src/Componentes/AboutUs/AboutUs.jsx
+++ frontend/src/Componentes/AboutUs/AboutUs.jsx
@@ -1,8 +1,9 @@
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
 import styles from "./AboutUs.module.css";
+import React from "react";
 import Nico from '../../Assets/Nico.jpg';
 import Dani from '../../Assets/Dani.jpg';
+import Jorge from '../../Assets/Jorge.jpeg';
+import German from '../../Assets/German.png';
 
 const AboutUs = () => {
   return (
@@ -10,40 +11,34 @@ const AboutUs = () => {
 
       <div className={styles.seccion1}>
         <div className={styles.seccion1_1}>
-          <CardGroup id='cartas'>
-            <Card>
-              <Card.Img variant="top" id={styles.fotos} classname={styles.fotos} src={Nico} />
-              <Card.Body>
-                <Card.Title>Nico</Card.Title>
-                <Card.Text>
-                  Texto descriptivo de cada integrante del grupo
-                </Card.Text>
-              </Card.Body>
-            </Card>
-            <Card>
-              <Card.Img variant="top" id={styles.fotos} classname={styles.fotos} src={Dani} />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  Texto descriptivo de cada integrante del grupo
-                </Card.Text>
-              </Card.Body>
-            </Card>
-            <Card>
-              <Card.Img variant="top" src="holder.js/100px160" />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural lead-in
-                  to additional content. This card has even longer content than the
-                  first to show that equal height action.
-                </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
-            </Card>
-          </CardGroup>
+          <ul>
+            <li>
+              <figure>
+                <img src={Nico} alt='Nico' />
+                <figcaption>
+                  <p>Texto descricion  Texto descricion Texto descricion Texto descricion </p>
+                </figcaption>
+              </figure>
+            </li>
+            <li>
+              <figure>
+                <img src={Dani} alt='Dani' />
+                <figcaption>Texto descricion Texto descricion Texto descricion Texto descricion </figcaption>
+              </figure>
+            </li>
+            <li>
+              <figure>
+                <img src={Jorge} alt='Jorge' />
+                <figcaption>Texto descricion Texto descricion Texto descricion </figcaption>
+              </figure>
+            </li>
+            <li>
+              <figure>
+                <img src={German} alt='German' />
+                <figcaption>Texto descricion Texto descricion Texto descricion </figcaption>
+              </figure>
+            </li>
+          </ul>
         </div>
 
       </div>
