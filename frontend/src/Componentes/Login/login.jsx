7559096839fabe8a28eac43fdf36ed2b8ce67e87
Germán Cachafeiro Bamonde <gercaba@mizu.local>
2023-01-25 21:01:14 +0100

    ajuste css del login

--- frontend/src/Componentes/Login/login.jsx
+++ frontend/src/Componentes/Login/login.jsx
@@ -4,46 +4,48 @@ import { setUserSession } from "../../utils/localStorageUtils";
 import styles from "./login.module.css"
 
 const Login = () => {
-    const [email,setEmail] = useState('')
-    const [password,setPassword] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-    const navigate = useNavigate()
+  const navigate = useNavigate()
   // fetch login
-  const fakeLogin = () => {
-    fetch("http://localhost:3001/login",{
-        method:'POST',
-        headers: {
-          Accept: "application/json",
-          "Content-type": "application/json",    //revisión
-          },
-        body:JSON.stringify({
-            email,
-            password
-        })
+  const sendLogin = () => {
+    fetch("http://localhost:3001/login", {
+      method: 'POST',
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password
+      })
     })
       .then((response) => {
         return response.json();
 
-      }).then((res)=>{
-        if(res.token){
-            setUserSession(res)
-            navigate('/')
-            
+      }).then((res) => {
+        if (res.token) {
+          setUserSession(res)
+          navigate('/')
+
         }
       })
   };
 
   return (
     <div className={styles.contenedor}>
+
       <div className={styles.title}>
         <h3>Login</h3>
       </div>
-      <div>
+
+      <div className={styles.formulario}>
         <label htmlFor="email">email</label>
-        <input type="text" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Email" />
+        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
         <label htmlFor="password">password</label>
-        <input type="text" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Password" />
-        <button onClick={() => fakeLogin()}>Login</button>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+        <button className={styles.botonLogin} onClick={() => sendLogin()}>Login</button>
       </div>
     </div>
   );
