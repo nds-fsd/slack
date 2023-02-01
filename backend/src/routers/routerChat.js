2cc1637fb44d39557cc990e2eb723dede8dc737b
Germán Cachafeiro Bamonde <gercaba@mizu.local>
2023-02-01 13:16:00 +0100

    comenzando el router de addUserChat

--- backend/src/routers/routerChat.js
+++ backend/src/routers/routerChat.js
@@ -23,14 +23,47 @@ routerChat.post("/createChat", jwtMiddleware, async (req, res) => {
     console.log("Entidad Chat", chat);
 
     //La forma de asignar el idOrganización es mediante una equivalencia cuando son dependencias 1 a N (sin array en el schema de Chat)
-    chat.organizacion = idOrganizacion
-    
+    chat.organizacion = idOrganizacion;
+
     //Esta es la forma de añadir usuarios al chat cuando son equivalencias N a N
     chat.user.push(idUser);
-  
+
     await chat.save();
 
     res.status(201).json(chat);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//Listar todos los chats
+routerChat.get("/allChats", jwtMiddleware, async (req, res) => {
+  try {
+    const allChats = await Chat.find();
+    res.status(200).json(allChats);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//Añadir user al chat en el cual siempre ya hay mínimo un usuario añadido que es el que crea el chat
+routerChat.patch("/addUserChat/:id", jwtMiddleware, async (req, res) => {
+  const idNewUser = req.body._id;
+  const idChat = req.params.id;
+
+  try {
+    const chatModified = await Chat.findById(idChat); //.populate('User');
+
+    const idExist = await Chat.findOne({user: idNewUser}).populate('user');
+
+    console.log('id Existe',idExist);
+
+    if (chatModified) {
+      chatModified.user.push(idNewUser);
+      res.status(200).json(chatModified);
+    } else {
+      res.status(404).send("Chat no encontrado");
+    }
   } catch (error) {
     res.status(500).json(error);
   }
