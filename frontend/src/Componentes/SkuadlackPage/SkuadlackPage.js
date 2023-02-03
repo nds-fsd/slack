2b3e6fa1390a9da6b401584db09a180827a91362
Daniel Castro Martin <109592821+DCM91@users.noreply.github.com>
2023-02-03 13:47:58 +0100

    espacios gestionados

--- frontend/src/Componentes/SkuadlackPage/SkuadlackPage.js
+++ frontend/src/Componentes/SkuadlackPage/SkuadlackPage.js
@@ -10,15 +10,43 @@ export const SkuadlackPage = () => {
       <div>fotoPerfil</div>
 
     </div>
+
     <div className='cuerpo'>
 
         <div className='box1'>
+          <div className='AddOrg'>+</div>
           <div className='Org'>Organizaciones</div>
           <div className='AddOrg'>+</div>
         </div>
-        
-        <div className='box2'>infOrg + chats + canales</div>
-        <div className='box3'>chats abiertos</div>
+
+        <div className='box2'>
+          <div className='chatbox'>infOrg</div>
+          <div className='chatbox'>canales </div>
+          <div className='chatbox'>chats</div>
+
+        </div>
+
+        <div className='box3'>
+
+          <div className='barraSuperiorChat'>
+            <div>nombre chat/user(s) con el que hablas</div>
+            <div>opciones </div>
+          </div>
+
+          <div className='bodyChat'>chat/canal abierto</div>
+
+          <div className='barraSuperiorChat'>
+            <div>input texto</div>
+            <div>enviar </div>
+          </div>
+        </div>
+
+
+        <div className='box1'>
+          <div className='AddOrg'>+</div>
+          <div className='Org'>users conectados</div>
+          <div className='AddOrg'>+</div>
+        </div>
     </div>
     </PageStyle>
   )
@@ -74,25 +102,66 @@ const PageStyle = styled.div `
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+    overflow: hidden;
 
     .Org{
-      height: 50%;
+      height: 80%;
       word-wrap: break-word;
+    }
+    .AddOrg{
+      height: 10%;
     }
   }
   .box2{
     position:static;
-    padding: 1rem;
+    padding: .1rem;
+    padding-bottom: 0.3rem;
     height: 100%;
     width: 30%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
     overflow: hidden;
+    text-align: center;
+
+    .chatbox{
+      box-shadow: 2px 2px 5px #0A0A0B, -2px -2px 5px #445CA3;
+      width: 100%;
+      height: 32%;
+
+    }
+
   }
   .box3{
-    padding: 1rem;
+    padding: .2rem;
     background-color: #f2f2f2;
     height: 100%;
     width: 65%;
     color: #090a0f;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    overflow: hidden;
+    
+    .barraSuperiorChat{
+      height: 8vh;
+      background-color: green;
+      color: #f2f2f2;
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      overflow: hidden;
+      align-items: center;
+      padding: 1rem;
+    }
+    .bodyChat{
+      background-color: yellowgreen;
+      height: 100%;
+
+
+    }
+  
   }
 }
 `
