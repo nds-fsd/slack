8ed647d8dc2ad55cd8fb5d7e42cb3a78fe484e6f
Jorge Cuesta <jorgecuesta.90@gmail.com>
2023-02-04 12:11:46 +0100

    add schema publicMessage.js

---
+++ backend/src/Schemas/publicMessage.js
@@ -0,0 +1,18 @@
+import {  model, Schema } from "mongoose";
+
+
+const PublicMessageSchema = new Schema(
+  {
+    text: String,
+    from: String,
+  },
+  {
+    timestamps: {
+      createdAt: "created_at", // Use `created_at` to store the created date
+    },
+  }
+);
+
+const PublicMessage = model("PublicMessage", PublicMessageSchema);
+
+export default PublicMessage;;
