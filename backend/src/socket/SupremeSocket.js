eece851e99147f4cf82cb83e4d8d199be8872574
Daniel Castro Martin <109592821+DCM91@users.noreply.github.com>
2023-02-17 12:11:06 +0100

    add coments

--- backend/src/socket/SupremeSocket.js
+++ backend/src/socket/SupremeSocket.js
@@ -4,6 +4,8 @@ import { Server } from 'socket.io';
 const app = express();
 const server = createServer(app);
 
+// Creamos el servidor de socket
+
 export const SupremeSocket = () => {
     const io = new Server(server, {
       cors: {
@@ -11,17 +13,29 @@ export const SupremeSocket = () => {
       },
     });
 
+// Manejamos los eventos de conexión de sockets
+
     io.on('connection', (socket) => {
         console.log('Usuario conectado al socket' + socket.id)
 
+// El usuario se une al canal "public" al conectarse
+
         socket.join('public')
+
+// Emitimos un mensaje a todos los usuarios cuando alguien se conecta
 
         io.emit('userConect',{
             from:'server',
             message:'Usuario conectado',
             server:'public'
         })
+
+// Manejamos el evento "chat", que se dispara cuando un usuario envía un mensaje
+
         socket.on('chat', (data)=>{
+
+                // Enviamos el mensaje a todos los sockets que estén en la misma sala (room)
+
             socket.to(data.roomId).emit('reply',{
                 from: socket.id,
                 message: data.message,
@@ -29,9 +43,14 @@ export const SupremeSocket = () => {
                 roomId: data.roomId
             })
         })
+
+// Manejamos el evento "joinRoom", que se dispara cuando un usuario se une a una sala
+
         socket.on('joinRoom',(data)=>{
+             // El usuario sale de la sala anterior
             socket.leave(data.previousRoom)
             console.log(`Usuario id: ${socket.id} en la room: ${data.room}`)
+            // El usuario se une a la nueva sala
             socket.join(data.room)
         })
     })
