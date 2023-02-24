ade55785ead78a96646be52af570e89876462d28
Jose Manuel Cano <jmcano94@gmail.com>
2023-02-24 10:43:16 +0100

    Replace autosize-textarea dependency for a newer one

--- frontend/src/Pages/private/ChatPage/AutoTextArea/autoTextArea.js
+++ frontend/src/Pages/private/ChatPage/AutoTextArea/autoTextArea.js
@@ -1,13 +1,15 @@
 import React from "react";
 import styles from './autoTextArea.module.css';
 
-import TextareaAutosize from 'react-autosize-textarea';
+import TextareaAutosize from 'react-textarea-autosize';
 
 const AutoTextArea = (props) => {
 	return (
 			<TextareaAutosize
 				{...props}
 				className={styles.textarea}
+				maxRows={3}
+				minRows={1}
 			/>
 	);
 };
