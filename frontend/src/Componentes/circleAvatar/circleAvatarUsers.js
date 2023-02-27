f898eb050fbc3b57bf34d53a4dd1b257da7a26f1
Jorge Cuesta <jorgecuesta.90@gmail.com>
2023-02-27 12:22:23 +0100

    agregar funcionalidad al circleAvatarUser, info del user q clickas en canvass

--- frontend/src/Componentes/circleAvatar/circleAvatarUsers.js
+++ frontend/src/Componentes/circleAvatar/circleAvatarUsers.js
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useSkuadLackContext } from "../../contexts/skuadLack-context"
 import fetchSupreme from '../../utils/apiWrapper';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 function CircleAvatarUsers({ name, color, size, id, path }) {
+  const [show, setShow] = useState(false);
+  const [userShow, setUserShow] = useState("")
+  const handleClose = () => setShow(false);
+  const handleShow = () => {
+    setShow(true)
+    fetchSupreme(`/user/${id}`, "GET", undefined, true, undefined)
+      .then((res) => {
+        setUserShow(res)
+
+      })
+  }
+
   // Extraer las dos primeras letras del nombre
 
-  const {idOrganizacionActual, setChats, chats, setRefreshContext, refreshContext, idUser } = useSkuadLackContext()
 
- const createChat= () => {
-    fetchSupreme(`/createChatById`, "POST", {organizacion: idOrganizacionActual, idUser: [id, idUser] }, true, undefined)
-    .then((res) => {
+
+  const { idOrganizacionActual, organizacionActual, setChats, chats, setRefreshContext, refreshContext, idUser } = useSkuadLackContext()
+
+  const createChat = () => {
+    fetchSupreme(`/createChatById`, "POST", { organizacion: idOrganizacionActual, idUser: [id, idUser] }, true, undefined)
+      .then((res) => {
         setRefreshContext(!refreshContext)
-        console.log(res)     
-
-    })
+      })
 
   }
 
   const initials = name
     .split(' ')
     .map(word => word[0])
+    .slice(0, 2)
     .join('')
     .toUpperCase();
 
@@ -42,8 +56,25 @@ function CircleAvatarUsers({ name, color, size, id, path }) {
 
 
   return (
-  
-  <button onClick={createChat} style={circleStyle}>{initials}</button>
+    <>
+      <Button onClick={handleShow} className="me-2"
+        style={circleStyle}>{initials}
+      </Button>
+      <Offcanvas show={show} placement="bottom" onHide={handleClose} >
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Usuario de {organizacionActual.OrgName} </Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <h3>{userShow.userName}</h3>
+          <h5>{userShow.name}</h5>
+          <h5>{userShow.lastName}</h5>
+          <h5>{userShow.email}</h5>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+
+
+
   )
 }
 
