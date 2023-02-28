f5c1799777fdcda41d4d129a426eadafe0f66e34
Daniel Castro Martin <109592821+DCM91@users.noreply.github.com>
2023-02-28 22:21:59 +0100

    merge de S5

--- frontend/src/Componentes/CloudinaryUpload/CloudinaryUpload.js
+++ frontend/src/Componentes/CloudinaryUpload/CloudinaryUpload.js
@@ -1,12 +1,12 @@
-import { CloudinaryContext } from 'cloudinary-react';
-import { useState } from 'react';
-import { Button } from 'react-bootstrap';
+import { CloudinaryContext } from "cloudinary-react";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
 import { AiOutlineUpload } from "react-icons/ai";
 
 export const CloudinaryUpload = () => {
   const [images, setImages] = useState([]);
   const [previewImages, setPreviewImages] = useState([]);
-  const url = 'https://api.cloudinary.com/v1_1/dnsy1t6dj/auto/upload';
+  const url = "https://api.cloudinary.com/v1_1/dnsy1t6dj/auto/upload";
 
   const handleImageUpload = async (event) => {
     const files = event.target.files;
@@ -16,9 +16,9 @@ export const CloudinaryUpload = () => {
     for (let i = 0; i < files.length; i++) {
       const file = files[i];
       const formData = new FormData();
-      formData.append('file', file);
-      formData.append('upload_preset', 'w6v9atp0');
-      const options = {method: 'POST', body: formData};
+      formData.append("file", file);
+      formData.append("upload_preset", "w6v9atp0");
+      const options = { method: "POST", body: formData };
       const response = await fetch(url, options);
       const json = await response.json();
       uploadedImages.push(json.secure_url);
@@ -41,43 +41,48 @@ export const CloudinaryUpload = () => {
 
   return (
     <CloudinaryContext
-      cloudName='dnsy1t6dj'
-      apiKey='573675825162776'
-      apiSecret='Q71fKH8AO31JQ-AILjoJx344_Eg'
+      cloudName="dnsy1t6dj"
+      apiKey="573675825162776"
+      apiSecret="Q71fKH8AO31JQ-AILjoJx344_Eg"
       secure={true}
     >
       <div>
-        <Button variant='dark' size='sm'
-        style={{
-                margin: '1rem'
-              }}>
-          <label htmlFor='image-upload'><AiOutlineUpload/></label>
+        <Button
+          variant="dark"
+          size="sm"
+          style={{
+            margin: "1rem",
+          }}
+        >
+          <label htmlFor="image-upload">
+            <AiOutlineUpload />
+          </label>
           <input
-            type='file'
-            id='image-upload'
+            type="file"
+            id="image-upload"
             onChange={handleImageUpload}
-            style={{ display: 'none' }}
+            style={{ display: "none" }}
             multiple
           />
         </Button>
       </div>
       <div>
         {previewImages.map((image, index) => (
-          <div key={index} style={{ position: 'relative' }}>
-            <img src={image} alt='Archivo Cargada' />
+          <div key={index} style={{ position: "relative" }}>
+            <img src={image} alt="Imagen Cargada" />
             <button
               style={{
-                position: 'absolute',
+                position: "absolute",
                 top: 0,
                 right: 0,
-                backgroundColor: 'transparent',
-                color: 'white',
-                border: 'none',
-                cursor: 'pointer',
-                fontSize: '1.2rem',
-                display: 'flex',
-                flexDirection: 'row',
-                width: '40%',
+                backgroundColor: "transparent",
+                color: "white",
+                border: "none",
+                cursor: "pointer",
+                fontSize: "1.2rem",
+                display: "flex",
+                flexDirection: "row",
+                width: "50%",
               }}
               onClick={() => handleImageRemove(index)}
             >
