b19df7c7332e3d59b6d2d2a356cdb3763f5c472a
Germán Cachafeiro Bamonde <gercaba@mizu.local>
2023-03-02 16:41:19 +0100

    añadir icono de delete y quitar código sin uso

--- frontend/src/Componentes/DeleteChat/deleteChat.jsx
+++ frontend/src/Componentes/DeleteChat/deleteChat.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useSkuadLackContext } from "../../contexts/skuadLack-context";
 import fetchSupreme from "../../utils/apiWrapper";
-import Button from 'react-bootstrap/Button';
 import styles from './deleteChat.module.css'
 import { BsTrash } from "react-icons/bs";
 
